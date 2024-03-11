bf9f4b07358bbc27fa557dd9213265036a2a7f5e
MadrinanComLab <johncliffordmadrinan838@gmail.com>
2024-03-11 15:30:01 +0800

    Homepage was personalized

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/images/clifford-head.png';
 import './App.css';
 
 function App() {
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Welcome to React GH Page<br/>Experiment!</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
